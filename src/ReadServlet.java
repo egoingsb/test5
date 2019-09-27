17bde875436eebac49123d769f72475a6347b60f
egoing <egoing@gmail.com>
2019-09-27 15:41:41 +0900

    first commit

---
+++ src/ReadServlet.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReadServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = req.getParameter("id");
+		String content = "";
+		if(id == null) {
+			id = "WEB";
+			content = "Welcome";
+		} else {
+			File file = new File("data/"+id);
+	        FileReader filereader = new FileReader(file);
+	        int singleCh = 0;
+	        while((singleCh = filereader.read()) != -1){
+	        	content += (char)singleCh;
+	        }
+	        filereader.close();
+		}
+
+		PrintWriter pw = resp.getWriter();
+		pw.write(""
+				+ "<html>"
+				+ "	<body>"
+				+ "		<h1><a href=\"read\">WEB</a></h1>"
+				+ "		<ol>"
+				+ "			<li><a href=\"read?id=HTML\">HTML</a></li>"
+				+ "			<li><a href=\"read?id=CSS\">CSS</a></li>"
+				+ "			<li><a href=\"read?id=JavaScript\">JavaScript</a></li>"
+				+ "		</ol>"
+				+ "		<h2>"+id+"</h2>"
+				+ "		"+content
+				+ "	</body>"
+				+ "</html>");
+		pw.close();
+	}
+
+}
